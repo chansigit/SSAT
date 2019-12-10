308c388793b369a36c16c54579daa2a9a853c34c
Sigit <chansigit@users.noreply.github.com>
2019-12-10 14:09:29 +0800

    Create annotate.genelist.R

---
+++ annotate.genelist.R
@@ -0,0 +1,28 @@
+load(url("https://github.com/chansigit/SSAT/raw/master/mm.cellsurfacemarker.rda"))
+load(url("https://github.com/chansigit/SSAT/raw/master/mm.secretory.rda"))
+load(url("https://github.com/chansigit/SSAT/raw/master/mm.tf.rda"))
+
+annotate.genelist<- function(markers, tf, surface, secretory, ...){
+    # annotate secretory protein
+    is.secretory <-as.character(markers$gene %in% secretory)
+    is.secretory <-replace(is.secretory, is.secretory=="TRUE",  "Secretory")
+    is.secretory <-replace(is.secretory, is.secretory=="FALSE", "")
+    
+    # annotate surface protein
+    is.surface <-as.character(markers$gene %in% surface)
+    is.surface <-replace(is.surface, is.surface=="TRUE",  "Surface")
+    is.surface <-replace(is.surface, is.surface=="FALSE", "")
+    
+    # annotate tf
+    is.tf <-as.character(markers$gene %in% tf)
+    is.tf <-replace(is.tf, is.tf=="TRUE",  "TF")
+    is.tf <-replace(is.tf, is.tf=="FALSE", "")
+    
+    markers$is.secretory <- is.secretory
+    markers$is.surface   <- is.surface
+    markers$is.tf        <- is.tf
+    
+    markers
+}
+
+annotate.genelist(seurat.deg.markers, tf=mm.tf, surface=mm.cellsurfacemarker, secretory=mm.secretory)
